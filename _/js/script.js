e34e6612138e9b50314ce6a489506d6ce29bc090
Kate Eldridge <kathryneeldridge@gmail.com>
2015-11-23 22:04:36 -0500

    skew tranform responsive until 772px

--- _/js/script.js
+++ _/js/script.js
@@ -1,1 +1,9 @@
-//scripts.js
+$( document ).ready(function() {
+	if($(window).width() > 399){
+		//alert("im working");
+		$('.hiring p').append('<span> new-dynamic-text</span>');
+	}else {
+		 $('.hiring p').remove('<span> new-dynamic-text</span>');
+	}//screen size
+
+});//document.ready

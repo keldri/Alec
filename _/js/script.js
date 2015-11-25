90cd3117580b5914529c899b5ded034cf0964dbf
Kate Eldridge <kathryneeldridge@gmail.com>
2015-11-25 04:45:58 -0500

    code cleanup

--- _/js/script.js
+++ _/js/script.js
@@ -3,17 +3,27 @@ $( document ).ready(function() {
 	$(window).resize(function() {
  	//check to see if window is greater than or less than 772px
 		  if($(window).width() > 772){
-	//if window is greater than 772, append .bottom div	to intro 	
+	//if window is greater than 772, hide register button, show form,append .bottom div	to intro	
+				$("#clicked").hide();
+				$(".input").show();
 				$('.bottom').appendTo('.intro'); 
-	//else if window is less than 772, append .bottom div to wrapper 	
+				$(".hiring p span").show();
+				//$(".hiring p span").text('Interested in being part of the team?').after("<br />");
+	//else if window is less than 772, append .bottom div to wrapper, return above to original state
 		   }else{
 				$('.bottom').appendTo('.wrapper'); 
-				
+				$("#clicked").show();
+				$(".input").hide();
+				$(".hiring p span").hide();
+				$(".hiring p span").empty();
 		   }
    });
-	$(".input").hide();
+	
+	//toggle show/hide of input form on click
 	$("#clicked").click(function(){
 		$(".input").toggle()
 	});
 
 });//document.ready*/
+
+//$.("hiring p span").text("Interested in being part of the team? ");
